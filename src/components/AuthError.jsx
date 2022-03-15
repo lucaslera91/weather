54be77b5ffa9724978b6cf24cbddc3aec7c46e51
lucaslera91 <lucaslera5@gmail.com>
2022-03-15 15:36:44 -0300

    links

--- src/components/AuthError.jsx
+++ src/components/AuthError.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function AuthError({user, noAuth}) {
   return (
     <div>
-        <div className='text-light'>
+        <div className='text-light p-3'>
             <h2 className='auth'>{user} {noAuth}</h2>
             <h5>En caso de Iphone - Instrucciones:</h5>
             <p>{'1- Ve a Ajustes > Privacidad > Localización.'}</p>
@@ -14,12 +14,12 @@ function AuthError({user, noAuth}) {
             <p>- Nunca: evita que pueda acceder a la información de Localización.</p>
             <p>- Preguntar la próxima vez: permite elegir Siempre, Cuando se use la app, Permitir una vez o No permitir.</p>
             <p>- Siempre: permite acceso a tu ubicación incluso mientras la app está en segundo plano.</p>
-            <p>Ayuda adicional: https://support.apple.com/es-es/HT207092</p>
+            <p>Ayuda adicional: <a href='https://support.apple.com/es-es/HT207092'>https://support.apple.com/es-es/HT207092</a></p>
             <p>Otros posibilidades:</p>
-            <p>Adnroid: https://support.google.com/nexus/answer/3467281?hl=es</p>
-            <p>Chrome: https://help.headspace.com/hc/es-419/articles/360046918374-Habilitar-la-configuraci%C3%B3n-de-ubicaci%C3%B3n-en-navegadores-de-dispositivos-m%C3%B3viles</p>
-            <p>FireFox:https://support.mozilla.org/en-US/kb/does-firefox-share-my-location-websites</p>
-            <p>Safari: https://support.apple.com/es-lamr/guide/mac-help/mh35873/mac</p>
+            <p><a href={'https://support.google.com/nexus/answer/3467281?hl=es'}>Adnroid</a></p>
+            <p><a href={'https://help.headspace.com/hc/es-419/articles/360046918374-Habilitar-la-configuraci%C3%B3n-de-ubicaci%C3%B3n-en-navegadores-de-dispositivos-m%C3%B3viles'}>Chrome</a></p>
+            <p><a href={'FireFox:https://support.mozilla.org/en-US/kb/does-firefox-share-my-location-websites'}>FireFox</a></p>
+            <p><a href={'https://support.apple.com/es-lamr/guide/mac-help/mh35873/mac'}>Safari</a></p>
         </div>
     </div>
   )
