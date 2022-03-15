a1c6ac24a6b8ba47d8793c4e5bba2c7dc5534a30
lucaslera91 <lucaslera5@gmail.com>
2022-03-15 18:54:06 -0300

    link

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 import './footer.css'
 
 function Footer() {
@@ -7,9 +6,9 @@ function Footer() {
     <div >
       <footer>
         <div className='main-div-footer'>
-          <Link className='my-auto' to={'https://github.com/lucaslera91'}><i className="fa-brands fa-github"></i></Link>
-          <Link className='my-auto' to={'https://www.google.com/search?q=Lucas+Lera+Ingeniero&rlz=1C5CHFA_enAR990AR990&sxsrf=APq-WBsuoUZwbdr4yk9N7V2_HRhSACBTSA%3A1647358566753&ei=ZrIwYtbILcjL1sQPhq680AY&ved=0ahUKEwjWk8HxuMj2AhXIpZUCHQYXD2oQ4dUDCA4&uact=5&oq=Lucas+Lera+Ingeniero&gs_lcp=Cgdnd3Mtd2l6EAMyBQghEKABMgUIIRCgATIECCEQFToHCCMQsAMQJzoHCAAQsAMQHjoJCAAQsAMQChAeOgkIABCwAxAFEB46CQgAELADEAgQHjoGCAAQFhAeOgcIIRAKEKABSgQIQRgBSgQIRhgAUPkBWIAOYMoRaAFwAHgAgAGTAogBsQuSAQUwLjkuMZgBAKABAcgBBsABAQ&sclient=gws-wiz'}><i className="fab fa-google-plus-g fa-lg white-text mr-md-5 mr-3 fa-2x"></i></Link>
-          <Link className='my-auto' to={'https://www.linkedin.com/in/lucas-lera-lls5/'}><i className="fab fa-linkedin-in fa-lg white-text mr-md-5 mr-3"></i></Link>
+          <a className='my-auto' href={'https://github.com/lucaslera91'}><i className="fa-brands fa-github"></i></a>
+          <a className='my-auto' href={'https://www.google.com/search?q=Lucas+Lera+Ingeniero&rlz=1C5CHFA_enAR990AR990&sxsrf=APq-WBsuoUZwbdr4yk9N7V2_HRhSACBTSA%3A1647358566753&ei=ZrIwYtbILcjL1sQPhq680AY&ved=0ahUKEwjWk8HxuMj2AhXIpZUCHQYXD2oQ4dUDCA4&uact=5&oq=Lucas+Lera+Ingeniero&gs_lcp=Cgdnd3Mtd2l6EAMyBQghEKABMgUIIRCgATIECCEQFToHCCMQsAMQJzoHCAAQsAMQHjoJCAAQsAMQChAeOgkIABCwAxAFEB46CQgAELADEAgQHjoGCAAQFhAeOgcIIRAKEKABSgQIQRgBSgQIRhgAUPkBWIAOYMoRaAFwAHgAgAGTAogBsQuSAQUwLjkuMZgBAKABAcgBBsABAQ&sclient=gws-wiz'}><i className="fab fa-google-plus-g fa-lg white-text mr-md-5 mr-3 fa-2x"></i></a>
+          <a className='my-auto' href={'https://www.linkedin.com/in/lucas-lera-lls5/'}><i className="fab fa-linkedin-in fa-lg white-text mr-md-5 mr-3"></i></a>
           
         </div>
       </footer>
